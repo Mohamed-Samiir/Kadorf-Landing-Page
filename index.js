02d4293ce8ab78b120db01d3722ac852ceec995a
Mohamed-Samiir <mohammedsamiir1996@gmail.com>
2023-10-13 16:26:47 +0300

    init

---
+++ index.js
@@ -0,0 +1,72 @@
+$(document).ready(function () {
+    $(".successStoris .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        nav: true,
+        mergeFit: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true,
+                center: true
+            },
+            600: {
+                items: 1,
+                nav: true
+            },
+            1000: {
+                items: 2,
+                nav: true,
+                loop: false
+            }
+        }
+    });
+    $(".featuredMembers .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        nav: true,
+        mergeFit: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: true
+            },
+            1000: {
+                items: 7,
+                nav: true,
+                loop: true,
+            }
+        }
+    });
+
+    $(".salesCycle .owl-carousel").owlCarousel({
+        loop: true,
+        margin: 20,
+        responsiveClass: true,
+        nav: true,
+        mergeFit: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: true
+            },
+            600: {
+                items: 3,
+                nav: true
+            },
+            1000: {
+                items: 4,
+                nav: true,
+                loop: true
+            }
+        }
+    });
+});
+
